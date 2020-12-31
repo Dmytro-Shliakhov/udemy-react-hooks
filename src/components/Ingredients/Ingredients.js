f063526d8c86a4b9b421317ef916e316c7e40330
dmytro.shliakhov <dmytro.shliakhov@altexsoft.com>
2020-12-31 13:14:36 +0200

    init

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch(
+      'https://udemy-react-hooks-20a1c-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json'
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
+    fetch(
+      'https://udemy-react-hooks-20a1c-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
+    fetch(
+      `https://udemy-react-hooks-20a1c-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.son`,
+      { method: 'DELETE' }
+    )
+      .then(() => {
+        setIsLoading(false);
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients.filter(({ id }) => id !== ingredientId),
+        ]);
+      })
+      .catch((e) => {
+        setError('Something went wrong!');
+        setIsLoading(false);
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="App">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
